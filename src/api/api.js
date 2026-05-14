572a74667864a55b37b5f685f88856826720cbdb
Gaurav <gourav4633@gmail.com>
2026-05-14 06:57:11 +0530

    backend3

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://future-bank-bachend-env.eba-3cnmzday.ap-south-1.elasticbeanstalk.com",
+  baseURL: "http://future-bank-bachend-env.eba-3cnmzday.ap-south-1.elasticbeanstalk.com/api/accounts",
 });
 
 export default API;
